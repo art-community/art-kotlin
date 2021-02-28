a12830ed6e3d97788b2cef54f904d952a65bcca3
antomy-gc <antomy-gc@gmail.com>
2021-02-28 04:23:38 +0300

    .

--- kotlin-extensions/src/main/kotlin/io/art/kotlin/extensions/model/storage/StorageModelConfiguratorExtension.kt
+++ kotlin-extensions/src/main/kotlin/io/art/kotlin/extensions/model/storage/StorageModelConfiguratorExtension.kt
@@ -5,7 +5,7 @@ import io.art.model.configurator.StorageModelConfigurator
 class StorageModelConfiguratorExtension(private val delegate: StorageModelConfigurator) :StorageModelConfigurator() {
 
 
-    fun tarantool(cluster: String?, configurator: TarantoolStorageConfiguratorExtension.() -> Unit) =
+    fun tarantool(cluster: String?, configurator: TarantoolStorageConfiguratorExtension.() -> Unit): StorageModelConfigurator =
             delegate.tarantool(cluster, { value -> TarantoolStorageConfiguratorExtension(value).apply(configurator) }) ?:
             delegate.tarantool ({ value -> TarantoolStorageConfiguratorExtension(value).apply(configurator) })
 
--- kotlin-extensions/src/main/kotlin/io/art/kotlin/extensions/model/storage/TarantoolStorageConfiguratorExtension.kt
+++ kotlin-extensions/src/main/kotlin/io/art/kotlin/extensions/model/storage/TarantoolStorageConfiguratorExtension.kt
@@ -1,26 +1,20 @@
 package io.art.kotlin.extensions.model.storage
 
-import io.art.model.configurator.StorageModelConfigurator
 import io.art.model.configurator.TarantoolSpaceModelConfigurator
 import io.art.model.configurator.TarantoolStorageModelConfigurator
-import kotlin.reflect.KClass
 
 class TarantoolStorageConfiguratorExtension(private val delegate: TarantoolStorageModelConfigurator) :TarantoolStorageModelConfigurator() {
-    inline fun <reified T, reified K> space(space: String, noinline configurator: TarantoolSpaceModelConfigurator.() -> Unit = {}) =
-            space(space, T::class, K::class, configurator)
+    inline fun <reified T, reified K> space(space: String, noinline configurator: TarantoolSpaceModelConfigurator<T, K>.() -> Unit = {}) =
+            space(space, T::class.java, K::class.java, configurator)
 
-    inline fun <reified T, reified K> space(space: String, vararg configurators: TarantoolSpaceModelConfigurator.() -> Unit) =
-            space(space, T::class, K::class, *configurators)
-
-
+    inline fun <reified T, reified K> space(space: String, vararg configurators: TarantoolSpaceModelConfigurator<T, K>.() -> Unit) =
+            space(space, T::class.java, K::class.java, *configurators)
 
 
+    fun <T, K> space(space: String, spaceModelClass: Class<T>, primaryKeyClass: Class<K>, configurator: TarantoolSpaceModelConfigurator<T, K>.() -> Unit = {}): TarantoolStorageModelConfigurator =
+            delegate.space(space, spaceModelClass, primaryKeyClass, { spaceConfig -> spaceConfig.apply(configurator) })
 
-    fun space(space:String, spaceModelClass: KClass<*>, primaryKeyClass: KClass<*>, configurator: TarantoolSpaceModelConfigurator.() -> Unit = {}) =
-            delegate.space(space, spaceModelClass.java, primaryKeyClass.java, { spaceConfig -> spaceConfig.apply(configurator) })
-
-    fun space(space:String, spaceModelClass: KClass<*>, primaryKeyClass: KClass<*>, vararg configurators: TarantoolSpaceModelConfigurator.() -> Unit) =
+    fun <T, K> space(space: String, spaceModelClass: Class<T>, primaryKeyClass: Class<K>, vararg configurators: TarantoolSpaceModelConfigurator<T, K>.() -> Unit) =
             configurators.forEach { configurator -> space(space, spaceModelClass, primaryKeyClass, configurator) }
-
 
 }
