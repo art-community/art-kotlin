8e61228e774cbb095853b475cc2122e9aecece7e
anton.bashirov <anton.sh.local@gmail.com>
2021-08-15 21:15:50 +0300

    ART

--- configurator/src/main/kotlin/io/art/configurator/kotlin/KotlinConfiguring.kt
+++ configurator/src/main/kotlin/io/art/configurator/kotlin/KotlinConfiguring.kt
@@ -3,5 +3,5 @@ package io.art.configurator.kotlin
 import io.art.configurator.Configuring.configuration
 import io.art.core.constants.StringConstants.EMPTY_STRING
 
-inline fun <reified T> T.configuration(): T = configuration(EMPTY_STRING)
-inline fun <reified T> T.configuration(section: String): T = configuration(section, T::class.java) as T
+inline fun <reified T> configuration(): T = configuration<T>(EMPTY_STRING)
+inline fun <reified T> configuration(section: String): T = configuration(section, T::class.java) as T
