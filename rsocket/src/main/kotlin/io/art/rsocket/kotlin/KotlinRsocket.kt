bbbf0960a080f930902fec23fe9eac01b9a5996f
anton.bashirov <anton.sh.local@gmail.com>
2021-08-15 23:55:56 +0300

    ART

--- rsocket/src/main/kotlin/io/art/rsocket/kotlin/KotlinRsocket.kt
+++ rsocket/src/main/kotlin/io/art/rsocket/kotlin/KotlinRsocket.kt
@@ -1,8 +1,19 @@
 package io.art.rsocket.kotlin
 
 import io.art.core.communication.Connector
+import io.art.meta.model.MetaClass
+import io.art.meta.model.MetaMethod
 import io.art.rsocket.Rsocket.rsocketConnector
+import io.art.rsocket.Rsocket.rsocketState
+import io.art.rsocket.state.RsocketModuleState.RsocketLocalState
 
 inline fun <reified T : Connector> rsocketConnector(): T = rsocketConnector(T::class.java) as T
 
 inline fun <reified T : Connector> rsocketConnector(action: T.() -> Any): Any = action(rsocketConnector(T::class.java) as T)
+
+
+inline fun <reified C, reified M : MetaClass<C>> rsocketState(crossinline method: (owner: M) -> MetaMethod<*>): RsocketLocalState = rsocketState(C::class.java) { owner: M -> method(owner) }
+
+inline fun <reified C, reified M : MetaClass<C>> rsocketState(crossinline method: (owner: M) -> MetaMethod<*>, action: RsocketLocalState.() -> Any) {
+    action(rsocketState(C::class.java) { owner: M -> method(owner) })
+}
