c7cf9e357eff6af2584374e9f61e539c742a1315
anton.bashirov <anton.sh.local@gmail.com>
2021-08-29 23:13:28 +0300

    ART

--- http/src/main/kotlin/io/art/http/kotlin/KotlinHttp.kt
+++ http/src/main/kotlin/io/art/http/kotlin/KotlinHttp.kt
@@ -6,6 +6,8 @@ import io.art.http.Http.httpState
 import io.art.http.state.HttpLocalState
 import io.art.meta.model.MetaClass
 import io.art.meta.model.MetaMethod
+import kotlin.properties.PropertyDelegateProvider
+import kotlin.properties.ReadOnlyProperty
 
 inline fun <reified T : Connector> httpConnector(): T = httpConnector(T::class.java) as T
 
@@ -17,3 +19,13 @@ inline fun <reified C, reified M : MetaClass<C>> httpState(crossinline method: (
 inline fun <reified C, reified M : MetaClass<C>> httpState(crossinline method: (owner: M) -> MetaMethod<*>, action: HttpLocalState.() -> Any) {
     action(httpState(C::class.java) { owner: M -> method(owner) })
 }
+
+val HttpLocalState.path
+    get() = PropertyDelegateProvider { _: Any?, _ ->
+        ReadOnlyProperty<Any?, String?> { _, property -> pathParameters()[property.name] }
+    }
+
+val HttpLocalState.query
+    get() = PropertyDelegateProvider { _: Any?, _ ->
+        ReadOnlyProperty<Any?, String?> { _, property -> queryParameters()[property.name] }
+    }
