11bd808b7e24371e6e5f0608bf4c2352ae96f071
anton.bashirov <anton.sh.local@gmail.com>
2021-09-20 02:33:18 +0300

    ART

--- tests/src/main/kotlin/io/art/tests/kotlin/KotlinTests.kt
+++ tests/src/main/kotlin/io/art/tests/kotlin/KotlinTests.kt
@@ -3,6 +3,7 @@ package io.art.tests.kotlin
 import io.art.launcher.Activator
 import io.art.tests.module.TestsActivator
 import io.art.tests.module.TestsInitializer
+import kotlin.reflect.KClass
 
 
 fun Activator.tests(configurator: TestsInitializer.() -> Any) {
@@ -12,3 +13,7 @@ fun Activator.tests(configurator: TestsInitializer.() -> Any) {
     }
     module(activator)
 }
+
+fun TestsInitializer.suit(suitClass: KClass<*>): TestsInitializer = suit(suitClass.java)
+
+inline fun <reified T> TestsInitializer.suit(): TestsInitializer = suit(T::class)
