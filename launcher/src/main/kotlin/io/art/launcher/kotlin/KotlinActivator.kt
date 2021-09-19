bdf7c7e012906b447b96efebde15272ca0f40a45
anton.bashirov <anton.sh.local@gmail.com>
2021-09-20 02:44:19 +0300

    ART

--- launcher/src/main/kotlin/io/art/launcher/kotlin/KotlinActivator.kt
+++ launcher/src/main/kotlin/io/art/launcher/kotlin/KotlinActivator.kt
@@ -1,24 +1,7 @@
 package io.art.launcher.kotlin
 
-import io.art.configurator.module.ConfiguratorActivator
-import io.art.configurator.module.ConfiguratorInitializer
-import io.art.http.module.HttpActivator
-import io.art.http.module.HttpInitializer
-import io.art.json.module.JsonActivator
 import io.art.launcher.Activator
 import io.art.launcher.Activator.activator
-import io.art.launcher.TestingLauncher
-import io.art.logging.module.LoggingActivator
-import io.art.logging.module.LoggingInitializer
-import io.art.message.pack.module.MessagePackActivator
-import io.art.meta.kotlin.KotlinMetaActivator
-import io.art.meta.model.MetaLibrary
-import io.art.meta.module.MetaInitializer
-import io.art.rsocket.module.RsocketActivator
-import io.art.rsocket.module.RsocketInitializer
-import io.art.scheduler.module.SchedulerActivator
-import io.art.transport.module.TransportActivator
-import io.art.yaml.module.YamlActivator
 
 fun activator(action: Activator.() -> Any) {
     action(activator())
@@ -27,16 +10,3 @@ fun activator(action: Activator.() -> Any) {
 fun activator(arguments: Array<String>, action: Activator.() -> Any) {
     action(activator(arguments))
 }
-
-fun testing(action: Activator.() -> Any) {
-    val activator = activator()
-    action(activator)
-    TestingLauncher.testing { activator }
-}
-
-fun testing(arguments: Array<String>, action: Activator.() -> Any) {
-    val activator = activator()
-    action(activator)
-    TestingLauncher.testing(arguments) { activator }
-}
-
