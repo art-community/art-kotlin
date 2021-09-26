5d279bfe3b7407da28c5fa51a44098f4ba347284
anton.bashirov <anton.sh.local@gmail.com>
2021-09-26 03:32:10 +0300

    ART

--- http/src/main/kotlin/io/art/http/kotlin/KotlinHttp.kt
+++ http/src/main/kotlin/io/art/http/kotlin/KotlinHttp.kt
@@ -9,7 +9,6 @@ import io.art.http.state.HttpLocalState
 import io.art.launcher.Activator
 import io.art.meta.model.MetaClass
 import io.art.meta.model.MetaMethod
-import kotlin.properties.PropertyDelegateProvider
 import kotlin.properties.ReadOnlyProperty
 
 inline fun <reified T : Connector> httpConnector(): T = httpConnector(T::class.java) as T
@@ -19,19 +18,14 @@ inline fun <reified T : Connector> httpConnector(action: T.() -> Any): Any = act
 
 inline fun <reified C, reified M : MetaClass<C>> httpState(crossinline method: (owner: M) -> MetaMethod<*>): HttpLocalState = httpState(C::class.java) { owner: M -> method(owner) }
 
-inline fun <reified C, reified M : MetaClass<C>> httpState(crossinline method: (owner: M) -> MetaMethod<*>, action: HttpLocalState.() -> Any) {
-    action(httpState(C::class.java) { owner: M -> method(owner) })
-}
+inline fun <reified C, reified M : MetaClass<C>, T> httpState(crossinline method: (owner: M) -> MetaMethod<*>, action: HttpLocalState.() -> T) =
+        action(httpState(C::class.java) { owner: M -> method(owner) })
 
 val HttpLocalState.path
-    get() = PropertyDelegateProvider { _: Any?, _ ->
-        ReadOnlyProperty<Any?, String?> { _, property -> pathParameters()[property.name] }
-    }
+    get() = ReadOnlyProperty<Any?, String?> { _, property -> pathParameters()[property.name] }
 
 val HttpLocalState.query
-    get() = PropertyDelegateProvider { _: Any?, _ ->
-        ReadOnlyProperty<Any?, String?> { _, property -> queryParameters()[property.name] }
-    }
+    get() = ReadOnlyProperty<Any?, String?> { _, property -> queryParameters()[property.name] }
 
 
 fun Activator.http(configurator: HttpInitializer.() -> Any) {
