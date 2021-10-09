924c9aaf658209afcdc451de74b37f0635cebdae
anton.bashirov <anton.sh.local@gmail.com>
2021-10-09 03:46:06 +0300

    ART

--- core/src/main/kotlin/io/art/core/kotlin/KotlinReactiveExtensions.kt
+++ core/src/main/kotlin/io/art/core/kotlin/KotlinReactiveExtensions.kt
@@ -3,8 +3,36 @@ package io.art.core.kotlin
 import io.art.core.extensions.ReactiveExtensions
 import reactor.core.publisher.Flux
 
+fun <T> Flux<T>.compensate(predicate: Boolean, compensation: Flux<T>): Flux<T> =
+        ReactiveExtensions.compensate(this, { predicate }, { compensation })
+
+fun <T> Flux<T>.compensate(predicate: Boolean, compensation: (T) -> Flux<T>): Flux<T> =
+        ReactiveExtensions.compensate(this, { predicate }, compensation)
+
+
 fun <T> Flux<T>.compensate(predicate: (T) -> Boolean, compensation: (T) -> Flux<T>): Flux<T> =
         ReactiveExtensions.compensate(this, predicate, compensation)
 
+fun <T> Flux<T>.compensate(predicate: (T) -> Boolean, compensation: Flux<T>): Flux<T> =
+        ReactiveExtensions.compensate(this, predicate) { compensation }
+
+
 fun <T> Flux<T>.compensateOnError(predicate: (Throwable) -> Boolean, compensation: (Throwable) -> Flux<T>): Flux<T> =
         ReactiveExtensions.compensateOnError(this, predicate, compensation)
+
+fun <T> Flux<T>.compensateOnError(predicate: (Throwable) -> Boolean, compensation: Flux<T>): Flux<T> =
+        ReactiveExtensions.compensateOnError(this, predicate) { compensation }
+
+
+fun <T> Flux<T>.compensateOnError(predicate: Boolean, compensation: (Throwable) -> Flux<T>): Flux<T> =
+        ReactiveExtensions.compensateOnError(this, { predicate }, compensation)
+
+fun <T> Flux<T>.compensateOnError(predicate: Boolean, compensation: Flux<T>): Flux<T> =
+        ReactiveExtensions.compensateOnError(this, { predicate }, { compensation })
+
+
+fun <T> Flux<T>.compensateOnError(compensation: (Throwable) -> Flux<T>): Flux<T> =
+        ReactiveExtensions.compensateOnError(this, compensation)
+
+fun <T> Flux<T>.compensateOnError(compensation: Flux<T>): Flux<T> =
+        ReactiveExtensions.compensateOnError(this) { compensation }
