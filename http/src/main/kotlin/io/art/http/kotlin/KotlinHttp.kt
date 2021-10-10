23732ebac728dd5ca6266e3b639311d35ddb9704
anton.bashirov <anton.sh.local@gmail.com>
2021-10-10 20:25:44 +0300

    ART

--- http/src/main/kotlin/io/art/http/kotlin/KotlinHttp.kt
+++ http/src/main/kotlin/io/art/http/kotlin/KotlinHttp.kt
@@ -1,11 +1,11 @@
 package io.art.http.kotlin
 
 import io.art.communicator.Connector
-import io.art.http.Http.httpConnector
-import io.art.http.Http.httpState
+import io.art.http.Http.*
 import io.art.http.module.HttpActivator.http
 import io.art.http.module.HttpInitializer
 import io.art.http.state.HttpLocalState
+import io.art.http.state.WsLocalState
 import io.art.launcher.Activator
 import io.art.meta.model.MetaClass
 import io.art.meta.model.MetaMethod
@@ -20,6 +20,12 @@ inline fun <reified C, reified M : MetaClass<C>> httpState(crossinline method: (
 
 inline fun <reified C, reified M : MetaClass<C>, T> httpState(crossinline method: (owner: M) -> MetaMethod<*>, action: HttpLocalState.() -> T) =
         action(httpState(C::class.java) { owner: M -> method(owner) })
+
+
+inline fun <reified C, reified M : MetaClass<C>> wsState(crossinline method: (owner: M) -> MetaMethod<*>): WsLocalState = wsState(C::class.java) { owner: M -> method(owner) }
+
+inline fun <reified C, reified M : MetaClass<C>, T> wsState(crossinline method: (owner: M) -> MetaMethod<*>, action: WsLocalState.() -> T) =
+        action(wsState(C::class.java) { owner: M -> method(owner) })
 
 val HttpLocalState.path
     get() = ReadOnlyProperty<Any?, String?> { _, property -> pathParameters()[property.name] }
