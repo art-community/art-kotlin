6217a9ecb457ebadc45c6da80d24232a19a97fad
anton.bashirov <anton.sh.local@gmail.com>
2022-03-30 19:46:01 +0300

    ART

--- http/src/main/kotlin/io/art/http/kotlin/KotlinHttp.kt
+++ http/src/main/kotlin/io/art/http/kotlin/KotlinHttp.kt
@@ -1,15 +1,16 @@
 package io.art.http.kotlin
 
-import io.art.communicator.model.ConnectorIdentifier
+import io.art.communicator.Communicator
+import io.art.http.Http
 import io.art.http.module.HttpActivator.http
 import io.art.http.module.HttpInitializer
 import io.art.http.state.HttpLocalState
 import io.art.launcher.Activator
 import kotlin.properties.ReadOnlyProperty
 
-inline fun <reified T : ConnectorIdentifier> http(): T = http(T::class.java) as T
+inline fun <reified T : Communicator> http(): T = Http.http(T::class.java) as T
 
-inline fun <reified T : ConnectorIdentifier> http(action: T.() -> Any): Any = action(http(T::class.java) as T)
+inline fun <reified T : Communicator> http(action: T.() -> Any): Any = action(Http.http(T::class.java) as T)
 
 val HttpLocalState.path
     get() = ReadOnlyProperty<Any?, String?> { _, property -> pathParameters()[property.name] }
--- rsocket/src/main/kotlin/io/art/rsocket/kotlin/KotlinRsocket.kt
+++ rsocket/src/main/kotlin/io/art/rsocket/kotlin/KotlinRsocket.kt
@@ -1,15 +1,14 @@
 package io.art.rsocket.kotlin
 
-import io.art.communicator.model.ConnectorIdentifier
+import io.art.communicator.Communicator
 import io.art.launcher.Activator
 import io.art.rsocket.Rsocket.rsocket
 import io.art.rsocket.module.RsocketActivator
 import io.art.rsocket.module.RsocketInitializer
 
-inline fun <reified T : ConnectorIdentifier> rsocket(): T = rsocket(T::class.java) as T
+inline fun <reified T : Communicator> rsocket(): T = rsocket(T::class.java) as T
 
-inline fun <reified T : ConnectorIdentifier> rsocket(action: T.() -> Any): Any = action(rsocket(T::class.java) as T)
-
+inline fun <reified T : Communicator> rsocket(action: T.() -> Any): Any = action(rsocket(T::class.java) as T)
 
 
 fun Activator.rsocket(configurator: RsocketInitializer.() -> Any = {}) {
