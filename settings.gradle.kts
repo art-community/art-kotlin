7c1c424cd378db2def5befeca2601375027387b0
anton <anton.sh.local@gmail.com>
2024-06-16 19:10:20 +0400

    try fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,7 +22,7 @@ pluginManagement {
     val internalPluginVersion: String by settings
     repositories {
         gradlePluginPortal()
-        maven { url = uri("https://nexus.art-platform.io/repository/art-gradle-plugins/") }
+        maven { url = uri("https://repsy.io/mvn/antonsh/art-packages/") }
     }
     resolutionStrategy {
         eachPlugin {
